aaadca16e1e4379d55b2835575481804e09ee486
Tafraouti Sanae <tafraoutist@gmail.com>
2025-04-30 16:07:26 +0100

    [LD-27] Portail Genie Informatique1

--- laravel/public/index.php
+++ laravel/public/index.php
@@ -1,4 +1,26 @@
 <?php
+<<<<<<< HEAD
 echo "<h1>Backend Test Réussi!</h1>";
 echo "<p>Date: ".date('Y-m-d H:i:s')."</p>";
 echo "<p>MySQL: ". (extension_loaded('pdo_mysql') ? '✔ Connecté' : '✖ Erreur')."</p>";
+=======
+require __DIR__.'/../vendor/autoload.php';
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+// Votre logique d'application ici
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Mon Application</title>
+    <link rel="stylesheet" href="/home/sanae/projet-docker/angular/src/styles.css">
+</head>
+<body>
+    <h1>Bienvenue sur mon application</h1>
+    
+    <script src="/home/sanae/projet-docker/laravel/public/js/app.js"></script>
+</body>
+</html>
+>>>>>>> c563d35 ([LD-27] Portail Genie Informatique1)
